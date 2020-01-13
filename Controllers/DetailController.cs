09578fea15e4e834a88e81764499eaab1328a1ff
Phitsanu Charoonsithong <nu198617@hotmail.com>
2020-01-13 14:22:52 +0700

    ....

---
+++ Controllers/DetailController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class DetailController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+
+        [HttpGet]
+        public string GetDetail()
+        {
+            Product product = new Product();
+
+            product.Name = "Apple";
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.Price = 3.99M;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+            return JsonConvert.SerializeObject(product);
+
+        }
+
+        
+    }
+
+    public class Product
+    {
+       
+
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public Decimal Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
